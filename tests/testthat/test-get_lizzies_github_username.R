778f2400625d7b8adca6dd8606327ff28683b3ce
Lizzie Roeble <lizzieroeble@Lizzies-MacBook-Pro.local>
2020-06-02 19:49:24 +0200

    Finish issue 12

--- tests/testthat/test-get_lizzies_github_username.R
+++ tests/testthat/test-get_lizzies_github_username.R
@@ -1,4 +1,4 @@
 test_that("use", {
-  skip("Issue 12, Issue #12")
-  expect_equal("no idea yet :-)", get_lizzys_github_username())
+  ("Issue 12, Issue #12")
+  expect_equal("Lizzie-Roeble", get_lizzie_github_username())
 })
