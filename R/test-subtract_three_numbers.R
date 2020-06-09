770b16b24c32679c96d9716a365e866d980cddc8
bogeert <bogeert@gmail.com>
2020-06-09 19:09:56 +0200

    Added test function test-subtract_three_numbers

---
+++ R/test-subtract_three_numbers.R
@@ -0,0 +1,3 @@
+test_that("addition by 3 works", {
+  expect_equal(3 - 3, 0)
+})
--- tests/testthat/test-get_geerts_github_username.R
+++ tests/testthat/test-get_geerts_github_username.R
@@ -1,4 +1,4 @@
 test_that("use", {
-  skip("Issue 12, Issue #12")
+  ("Issue 12, Issue #12")
   expect_equal("bogeert", get_geerts_github_username())
 })
