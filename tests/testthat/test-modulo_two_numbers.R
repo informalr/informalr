71806b338546ff01490ff30eb914b9ceb7b4bb79
Willy Tadema <friesewoudloper@gmail.com>
2020-06-10 14:16:37 +0200

    Fixed lintr bot issues

--- tests/testthat/test-modulo_two_numbers.R
+++ tests/testthat/test-modulo_two_numbers.R
@@ -3,12 +3,12 @@ test_that("use", {
   expect_equal(modulo_two_numbers(0, 15), 0)
   expect_equal(modulo_two_numbers(4, 5), 4)
   expect_equal(modulo_two_numbers(5.37, 1.45), 1.02)
-  expect_equal(modulo_two_numbers(13, 3), 1)            # https://torstencurdt.com/tech/posts/modulo-of-negative-numbers/
+  expect_equal(modulo_two_numbers(13, 3), 1)
   expect_equal(modulo_two_numbers(-13, 3), 2)
   expect_equal(modulo_two_numbers(13, -3), -2)
   expect_equal(modulo_two_numbers(-13, -3), -1)
   expect_true(is.na(modulo_two_numbers(1, 0)))
-  expect_error(modulo_two_numbers('a', 'b'), "is.numeric(a) & is.numeric(b) is not TRUE", fixed = TRUE)
+  expect_error(modulo_two_numbers("a", "b"),
+               "is.numeric(a) & is.numeric(b) is not TRUE",
+               fixed = TRUE)
 })
-
-
