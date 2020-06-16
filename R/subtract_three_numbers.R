563fd534d10dc1f64549f4dc8666df87c3b3a899
bogeert <bogeert@gmail.com>
2020-06-16 19:17:16 +0200

    #12 functie

---
+++ R/subtract_three_numbers.R
@@ -0,0 +1,12 @@
+#' Add up two numbers
+#'
+#' @param a A number
+#' @return 3 from a subtracted
+#' @examples
+#' library(testthat)
+#' expect_equal(5, subtract_three_numbers(2))
+#' @author Geert van den Bogaart
+#' @export
+add_three_numbers <- function(a) {
+  a - 3
+}
