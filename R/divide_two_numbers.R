53f7dde3890846b2221b9527e6731d083d97ff11
Neves-P <p.m.santos.neves@rug.nl>
2020-06-16 19:00:07 +0200

    Add divide_two_numbers. Progress #32

---
+++ R/divide_two_numbers.R
@@ -0,0 +1,16 @@
+#' Divide two numbers
+#'
+#' @param a A number
+#' @param b A number
+#'
+#' @return The division of number `a` by number `b`.
+#' @export
+#'
+#' @examples
+#' testthat::expect_equal(
+#'   divide_two_numbers(a = 2, b = 5),
+#'   0.4
+#' )
+divide_two_numbers <- function(a, b) {
+  a / b
+}
--- tests/testthat/test-divide_two_numbers.R
+++ tests/testthat/test-divide_two_numbers.R
@@ -1,4 +1,3 @@
 test_that("divide_two_numbers use", {
-  skip("Issue 31")
   expect_equal(divide_two_numbers(4, 2), 2)
 })
