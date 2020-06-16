c69a0913c743af4fd772a3ccc9735477d7c3f526
swom <cicciacco@gmail.com>
2020-06-16 18:56:52 +0200

    adding trivial function #12

---
+++ R/get_stefanos_github_username.R
@@ -0,0 +1,12 @@
+#' Get Stefano's GitHub username
+#' @return Stefano's GitHub username
+#' @examples
+#' library(testthat)
+#'
+#' username <- get_stefanos_github_username()
+#' expect_equal("swom", username)
+#' @author Stefano Tiso
+#' @export
+get_stefanos_github_username <- function() {
+  "swom"
+}
