33686b93c0bf7ed3a49afe016b72b6597c04a240
Willy Tadema <friesewoudloper@gmail.com>
2020-06-23 18:56:50 +0200

    Add test for function misuse, progress #34

--- tests/testthat/test-modulo_two_numbers.R
+++ tests/testthat/test-modulo_two_numbers.R
@@ -8,7 +8,11 @@ test_that("use", {
   expect_equal(modulo_two_numbers(13, -3), -2)
   expect_equal(modulo_two_numbers(-13, -3), -1)
   expect_true(is.na(modulo_two_numbers(1, 0)))
-  expect_error(modulo_two_numbers("a", "b"),
-               "is.numeric(a) & is.numeric(b) is not TRUE",
-               fixed = TRUE)
+})
+
+test_that("abuse", {
+  expect_error(modulo_two_numbers("a", 42),
+               "The first argument is not a number")
+  expect_error(modulo_two_numbers(42, "b"),
+               "The second argument is not a number")
 })
