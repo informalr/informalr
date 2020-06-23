6d1f727f8e5dd2287effb099c096e5fcc3f93411
bogeert <bogeert@gmail.com>
2020-06-23 19:03:05 +0200

    error stuff added

--- tests/testthat/test-subtract_three_numbers.R
+++ tests/testthat/test-subtract_three_numbers.R
@@ -2,3 +2,10 @@ test_that("subtraction by 3 works", {
 
   expect_equal(subtract_three_numbers(5), 2)
 })
+
+test_that("use", {
+  expect_error(
+    subtract_three_numbers("nonsense"),
+    "Argument must be a number"
+  )
+  })
