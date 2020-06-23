25f8ea685146b08d48d4d95df88ad1f50f408fa7
fastnotfurious <close_watch@posteo.de>
2020-06-23 19:34:56 +0200

    fixing issue - sorry

--- tests/testthat/test-add_three_numbers.R
+++ tests/testthat/test-add_three_numbers.R
@@ -4,15 +4,15 @@ test_that("add_three_numbers", {
 
 test_that("add_three_numbers", {
   expect_error(
-    add_two_numbers("nonsense", 3, 3),
+    add_three_numbers("nonsense", 3, 3),
     "First argument must be a number"
   )
   expect_error(
-    add_two_numbers(42, "nonsense", 44),
+    add_three_numbers(42, "nonsense", 44),
     "Second argument must be a number"
   )
   expect_error(
-    add_two_numbers(5, 5, "nonsense"),
+    add_three_numbers(5, 5, "nonsense"),
     "Last argument must be a number"
   )
 })
