7423d4645b89250e11b0d3cd9b6f88ce867cc4dd
janclod <ctiecher@gmail.com>
2020-08-25 20:33:36 +0200

    Fixed test for function get_claudios_github_username

--- R/get_claudios_github_username.R
+++ R/get_claudios_github_username.R
@@ -3,7 +3,7 @@
 #' @examples
 #' library(testthat)
 #'
-#' username <- get_richels_github_username()
+#' username <- get_claudios_github_username()
 #' expect_equal("janclod", username)
 #' @author Claudio Tiecher
 #' @export
