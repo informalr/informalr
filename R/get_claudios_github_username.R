29f599d837a0ae8c9547c5729ffa8c75d81a22ce
janclod <ctiecher@gmail.com>
2020-08-25 19:54:14 +0200

    Fixed issue #39

---
+++ R/get_claudios_github_username.R
@@ -0,0 +1,13 @@
+#' Get Claudio's GitHub username
+#' @return Claudio's GitHub username
+#' @examples
+#' library(testthat)
+#'
+#' username <- get_richels_github_username()
+#' expect_equal("janclod", username)
+#' @author Claudio Tiecher
+#' @export
+get_claudios_github_username <- function() {
+  "janclod"
+}
+
