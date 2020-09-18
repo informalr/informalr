add307ff73061b83147ba5d0651a6d36b228a519
Willy Tadema <friesewoudloper@gmail.com>
2020-09-18 17:56:34 +0200

    Added create_map function, closes #60

--- R/create_map.R
+++ R/create_map.R
@@ -1,17 +1,13 @@
-#' Create a map
+#' Create map
 #'
-#' Creates a map with the desired hiking info.
-#' The map is saved as a PNG file.
+#' Fetch a map of Groningen from OpenStreetMap
+#' and save it as a PNG-file.
+#'
 #' @param png_filename name of the PNG that the map will be saved to
 #' @export
 create_map <- function(png_filename) {
-  # Stub: just copy a file
-  file.copy(
-    from = system.file(
-      "extdata",
-      "map.png",
-      package = "informalr"
-    ),
-    to = png_filename
-  )
+  bbox <- osmdata::getbb("Groningen")
+  groningen <- ggmap::get_map(bbox, maptype = "toner-background")
+  p <- ggmap::ggmap(groningen) + ggthemes::theme_map()
+  ggplot2::ggsave(filename = png_filename, plot = p)
 }
--- tests/testthat/test-get_real_map.R
+++
@@ -1,5 +0,0 @@
-test_that("use", {
-  skip("Issue #64")
-  get_real_map()
-  expect_true(file.exists("./inst/extdata/groningen.png"))
-})
