65f9e99c237749a88285b092f108d4afaef3f095
swom <cicciacco@gmail.com>
2020-09-22 18:47:25 +0200

    trying to fix conflicts

--- R/divide_two_numbers.R
+++ R/divide_two_numbers.R
@@ -2,7 +2,6 @@
 #'
 #' @param a A number
 #' @param b A number
-<<<<<<< HEAD
 #' @return The result of the division of \code{a} and \code{b}
 #' @examples
 #' divide_two_numbers(4, 2)
@@ -11,19 +10,5 @@
 #' @export
 divide_two_numbers <- function(a, b) {
   stopifnot(is.numeric(a) & is.numeric(b))
-=======
-#'
-#' @return The division of number `a` by number `b`.
-#' @export
-#'
-#' @examples
-#' testthat::expect_equal(
-#'   divide_two_numbers(a = 2, b = 5),
-#'   0.4
-#' )
-#'
-#' @author Pedro
-divide_two_numbers <- function(a, b) {
->>>>>>> develop
   a / b
 }
