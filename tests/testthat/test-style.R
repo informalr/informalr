48ec850cd7b39243bc53c504a3a054196d1bfc9a
richelbilderbeek <richel@richelbilderbeek.nl>
2020-10-02 11:35:41 +0200

    Get lintr to complain, using https://github.com/jimhester/lintr/issues/421#issuecomment-559267922

--- tests/testthat/test-style.R
+++ tests/testthat/test-style.R
@@ -1,3 +1,19 @@
 test_that("Package style", {
-  lintr::expect_lint_free()
+
+ # A duplicate copy of the find_package function from lintr
+ find_package <- function(path) {
+   path <- normalizePath(path, mustWork = FALSE)
+
+   while (!file.exists(file.path(path, "DESCRIPTION"))) {
+      path <- dirname(path)
+      if (identical(path, dirname(path))) {
+        return(NULL)
+      }
+    }
+    path
+  }
+
+  if (!is.null(find_package("."))) {
+    lintr::expect_lint_free()
+  }
 })
