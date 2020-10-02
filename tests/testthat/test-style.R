49064e53ac28df5d877a10651655afc1a7ba3a57
richelbilderbeek <richel@richelbilderbeek.nl>
2020-10-02 11:56:06 +0200

    Simplify style test

--- tests/testthat/test-style.R
+++ tests/testthat/test-style.R
@@ -1,19 +1,3 @@
 test_that("Package style", {
-
- # A duplicate copy of the find_package function from lintr
- find_package <- function(path) {
-   path <- normalizePath(path, mustWork = FALSE)
-
-   while (!file.exists(file.path(path, "DESCRIPTION"))) {
-      path <- dirname(path)
-      if (identical(path, dirname(path))) {
-        return(NULL)
-      }
-    }
-    path
-  }
-
-  if (!is.null(find_package("."))) {
-    lintr::expect_lint_free()
-  }
+  lintr::expect_lint_free()
 })
