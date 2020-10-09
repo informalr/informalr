74ea9673574109e9d3f880935669579930843926
richelbilderbeek <richel@richelbilderbeek.nl>
2020-10-09 12:56:04 +0200

    'create_map' creates a PNG, fix #64

--- tests/testthat/test-create_map.R
+++ tests/testthat/test-create_map.R
@@ -1,6 +1,6 @@
 test_that("use", {
   skip("Issue 64, test not working properly")
-  png_filename <- tempfile()
+  png_filename <- tempfile(fileext = ".png")
   create_map(png_filename = png_filename)
   expect_true(file.exists(png_filename))
 })
