7c900f77ce371d3f0a9c2aaf8ab47ac94f56c01b
Frans Bianchi <fransbianchi@gmail.com>
2020-10-20 19:38:12 +0200

    abuse done

--- tests/testthat/test-substract_four_numbers.R
+++ tests/testthat/test-substract_four_numbers.R
@@ -2,7 +2,7 @@ test_that("subtraction by 4 works", {
   expect_equal(subtract_four_numbers(5), 1)
 })
 
-test_that("use", {
+test_that("abuse", {
   expect_error(
     subtract_four_numbers("nonsense"),
     "Argument must be a number"
