fd1a0fcd2cf4fc7b5444e0f2e57ff0f5291ce8b0
swom <cicciacco@gmail.com>
2020-10-20 19:43:08 +0200

    adding 'brutalize' test to check for correct input to divide_two_numbers function

--- tests/testthat/test-divide_two_numbers.R
+++ tests/testthat/test-divide_two_numbers.R
@@ -1,4 +1,31 @@
 
 test_that("divide_two_numbers use", {
-  expect_equal(divide_two_numbers(4, 2), 2)
+    expect_equal(divide_two_numbers(4, 2), 2)
+})
+
+test_that("brutalize", {
+  skip("Issue #34")
+  expect_silent(divide_two_numbers(4, 2))
+
+  expect_error(divide_two_numbers(dog, 1),
+               "The first argument is not a number")
+
+  expect_error(divide_two_numbers(1, dog),
+               "The second argument is not a number")
+
+  expect_error(divide_two_numbers(1, NA),
+               "The second argument is NA")
+
+  expect_error(divide_two_numbers(NA, 1),
+               "The first argument is NA")
+
+  expect_error(divide_two_numbers(1, Inf),
+               "The second argument is infinity")
+
+  expect_error(divide_two_numbers(Inf, 1),
+               "The first argument is infinity")
+
+  expect_error(divide_two_numbers(2, 0),
+               "Cannot divide by 0!")
+
 })
