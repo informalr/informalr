c5fc2851c1a7b926ead225378aac61d6a25c6e1a
richelbilderbeek <richel@richelbilderbeek.nl>
2020-10-20 16:49:24 +0200

    Undo demo of https://youtu.be/Qn_UYBz9k2w

--- tests/testthat/test-create_map.R
+++ tests/testthat/test-create_map.R
@@ -4,12 +4,3 @@ test_that("use", {
   expect_silent(create_map(png_filename = png_filename))
   expect_true(file.exists(png_filename))
 })
-
-test_that("display bridges", {
-  png_filename <- tempfile(fileext = ".png")
-  create_map(
-    png_filename = png_filename,
-    show_bridges = TRUE
-  )
-
-})
