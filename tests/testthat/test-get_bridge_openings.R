c530a4ad73da1616aa60650a8e8c39d86885b268
janclod <ctiecher@gmai.com>
2020-10-20 19:18:12 +0200

    Added tests to get_bridge_openings()

--- tests/testthat/test-get_bridge_openings.R
+++ tests/testthat/test-get_bridge_openings.R
@@ -1,3 +1,15 @@
 test_that("use", {
   expect_true(nrow(get_bridge_openings()) > 0)
 })
+
+test_that("longitude", {
+  bridges <- get_bridge_openings()
+  expect_true(as.double(bridges$lon)[1] <= 180)
+  expect_true(as.double(bridges$lon)[1] >= -180)
+})
+
+test_that("latitude", {
+  bridges <- get_bridge_openings()
+  expect_true(as.double(bridges$lat)[1] <= 90)
+  expect_true(as.double(bridges$lat)[1] >= 0)
+})
