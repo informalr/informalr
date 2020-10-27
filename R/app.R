ce6c899fdf3d4d684ccacbdaf3b27ff100070d12
janclod <ctiecher@gmail.com>
2020-10-27 20:03:55 +0100

    Simplified Shiny app look

--- R/app.R
+++ R/app.R
@@ -1,49 +1,28 @@
+#' Shiny App
+#'
+#' @examples
+#' run_app()
+#' @author Claudio Tiecher
+#' @export
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
+# This is a Shiny web application.
 #
 # Find out more about building applications with Shiny here:
 #
 #    http://shiny.rstudio.com/
 #
 
-# Define UI for application that draws a histogram
-ui <- shiny::fluidPage(
-
-    # Application title
-    shiny::titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    shiny::sidebarLayout(
-        shiny::sidebarPanel(
-            shiny::sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        shiny::mainPanel(
-           shiny::plotOutput("distPlot")
-        )
-    )
-)
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$dist_plot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = "darkgray", border = "white")
-    })
-}
-
 # Run the application
 run_app <- function() {
+    # Define UI for application that draws a histogram
+    ui <- shiny::fluidPage(
+
+        # Application title
+        shiny::titlePanel("Hiking guide")
+    )
+
+    # Define server logic required to draw a histogram
+    server <- function(input, output) {
+    }
     shiny::shinyApp(ui = ui, server = server)
 }
