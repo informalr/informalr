ae5b7d9aa02b14bb3265a0fcb1f465a99f4a9ee7
richelbilderbeek <richel@richelbilderbeek.nl>
2020-11-03 19:16:11 +0100

    Shiny app progress

--- R/app.R
+++ R/app.R
@@ -18,11 +18,17 @@ run_app <- function() {
     ui <- shiny::fluidPage(
 
         # Application title
-        shiny::titlePanel("Hiking guide")
+        shiny::titlePanel("Hiking guide"),
+        shiny::imageOutput("map")
     )
 
     # Define server logic required to draw a histogram
     server <- function(input, output) {
+      output$map <- renderPlot({
+        png_filename <- tempfile(fileext = ".png")
+        informalr::create_map(png_filename)
+        png(png_filename)
+      })
     }
     shiny::shinyApp(ui = ui, server = server)
 }
