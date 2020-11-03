f1594a893ed8dfbbd4501b50f73ebd804c3ddd5d
bogeert <bogeert@gmail.com>
2020-11-03 19:16:17 +0100

    #88 b

--- R/create_map.R
+++ R/create_map.R
@@ -8,13 +8,13 @@
 create_map <- function(png_filename, show_bridge_openings=TRUE) {
   bbox <- osmdata::getbb("Groningen", featuretype = "state")
   groningen <- ggmap::get_map(bbox, maptype = "toner_stamen")
-  p <- ggmap::ggmap(groningen) + ggthemes::theme_map()
+  p <- ggmap::ggmap(groningen)
   if(show_bridge_openings) {
     data <- NULL; rm(data) # nolint, fixes warning: no visible  binding
     data <- informalr::get_bridge_openings()
     data$lat <- as.numeric(data$lat)
     data$lon <- as.numeric(data$lon)
-    p <- p   +
+    p <- p  +
       ggplot2::geom_point(data = data,
                           ggplot2::aes(x = data$lon, y = data$lat),
                           colour = I("red"), size = I(3))}
