afeda3b2d21fa21877d6f85d271746260b062586
Willy Tadema <friesewoudloper@gmail.com>
2020-11-04 23:58:20 +0100

    No more warnings because of POI's outside the bounding box of the map

--- R/create_map.R
+++ R/create_map.R
@@ -6,14 +6,18 @@
 #' @param png_filename name of the PNG that the map will be saved to
 #' @export
 create_map <- function(png_filename, show_bridge_openings = TRUE) {
-  bbox <- osmdata::getbb("Nederland", featuretype = "country")
-  nl <- ggmap::get_map(bbox, maptype = "toner_stamen", quiet = TRUE)
-  p <- ggmap::ggmap(nl)
+  bbox <- osmdata::getbb("Groningen", featuretype = "state")
+  groningen <- ggmap::get_map(bbox, maptype = "toner_stamen", quiet = TRUE)
+  p <- ggmap::ggmap(groningen)
   if (show_bridge_openings) {
     data <- NULL; rm(data) # nolint, fixes warning: no visible  binding
     data <- informalr::get_bridge_openings()
     data$lat <- as.numeric(data$lat)
     data$lon <- as.numeric(data$lon)
+    data <- data[data$lat >= bbox['y', 'min'] &
+                 data$lat <= bbox['y', 'max'] &
+                 data$lon >= bbox['x', 'min'] &
+                 data$lon <= bbox['x', 'max'], ]
     p <- p  +
       ggplot2::geom_point(data = data,
                           ggplot2::aes(x = lon, y = lat),
