fe644716e574c002a78bfd9acacb2ba3f57f16f6
Willy Tadema <friesewoudloper@gmail.com>
2020-11-04 22:50:40 +0100

    Undo skip test

--- tests/testthat/test-create_map.R
+++ tests/testthat/test-create_map.R
@@ -1,5 +1,4 @@
 test_that("use", {
-  skip("Issue 88")
   png_filename <- tempfile(fileext = ".png")
   expect_silent(
     create_map(
@@ -8,6 +7,7 @@ test_that("use", {
     )
   )
 
+  expect_true(file.exists(png_filename))
   expect_silent(
     create_map(
       show_brige_openings = FALSE,
