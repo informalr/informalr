633382a1f0915676832b8f7b85cc72ab96ee7206
richelbilderbeek <richel@richelbilderbeek.nl>
2020-11-10 13:54:44 +0100

    Shiny app does not work, but does not cause the build to break either

--- R/app.R
+++ R/app.R
@@ -19,15 +19,16 @@ run_app <- function() {
 
         # Application title
         shiny::titlePanel("Hiking guide"),
-        shiny::imageOutput("map")
+        shiny::imageOutput("map", width = "400px", height = "400px")
     )
 
-    # Define server logic required to draw a histogram
+    # Define server logic required to draw the PNG
+    # Does not work :-(
     server <- function(input, output) {
-      output$map <- renderPlot({
+      output$map <- shiny::renderPlot({
         png_filename <- tempfile(fileext = ".png")
         informalr::create_map(png_filename)
-        png(png_filename)
+        grDevices::png(png_filename, width = 400, height = 400, type = "cairo")
       })
     }
     shiny::shinyApp(ui = ui, server = server)
