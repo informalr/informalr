a475b8c7d614a32840e273d018eac49bbbcf2a60
richelbilderbeek <richel@richelbilderbeek.nl>
2020-11-10 15:14:04 +0100

    Expose #101

--- tests/testthat/test-create_map.R
+++ tests/testthat/test-create_map.R
@@ -31,12 +31,100 @@ test_that("show a certain rectangle of the map", {
   )
 })
 
-test_that("show show bike traffic density", {
+test_that("show bike traffic density", {
   skip("Issue 97. Issue #97")
   expect_silent(
     create_map(
       png_filename = png_filename,
       show_bike_traffic_density = TRUE
     )
+  )
+})
+
+test_that("show car traffic density", {
+  skip("Issue 98. Issue #98")
+  expect_silent(
+    create_map(
+      png_filename = png_filename,
+      show_car_traffic_density = TRUE
+    )
+  )
+})
+
+test_that("show accidents", {
+  skip("Issue 101. Issue #101")
+  expect_silent(
+    create_map(
+      png_filename = png_filename,
+      show_accidents = TRUE
+    )
+  )
+})
+
+test_that("check png_filename argument", {
+  skip("Issue 99. Issue #99")
+  expect_error(
+    create_map(
+      png_filename = 42
+    ),
+    "'png_filename' must be one word"
+  )
+  expect_error(
+    create_map(
+      png_filename = c("one filename", "too many")
+    ),
+    "'png_filename' must be one word"
+  )
+  expect_error(
+    create_map(
+      png_filename = NULL
+    ),
+    "'png_filename' must be one word"
+  )
+  expect_error(
+    create_map(
+      png_filename = c()
+    ),
+    "'png_filename' must be one word"
+  )
+  expect_error(
+    create_map(
+      png_filename = NA
+    ),
+    "'png_filename' must be one word"
+  )
+})
+
+test_that("check show_bridge_openings argument", {
+  skip("Issue 100 Issue #100")
+  expect_error(
+    create_map(
+      show_bridge_openings = 42
+    ),
+    "'show_bridge_openings' must be TRUE or FALSE"
+  )
+  expect_error(
+    create_map(
+      show_bridge_openings = c(TRUE, FALSE)
+    ),
+    "'show_bridge_openings' must be TRUE or FALSE"
+  )
+  expect_error(
+    create_map(
+      show_bridge_openings = NULL
+    ),
+    "'show_bridge_openings' must be TRUE or FALSE"
+  )
+  expect_error(
+    create_map(
+      show_bridge_openings = c()
+    ),
+    "'show_bridge_openings' must be TRUE or FALSE"
+  )
+  expect_error(
+    create_map(
+      show_bridge_openings = NA
+    ),
+    "'show_bridge_openings' must be TRUE or FALSE"
   )
 })
