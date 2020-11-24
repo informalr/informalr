86da9ddb2155e2e912defdb586bf1de8d64ec3f2
Willy Tadema <friesewoudloper@gmail.com>
2020-11-24 19:05:46 +0100

    Add check_show_bridge_openings

---
+++ R/check_show_bridge_openings.R
@@ -0,0 +1,9 @@
+#' Check the value of `show_bridge_openings`
+#' @param show_bridge_openings character string indicating whether or not
+#' bridge openings are plotted on top of the basemap.
+#' @export
+check_show_bridge_openings <- function(show_bridge_openings) {
+  '%ni%' <- Negate('%in%')
+  if (!is.character(show_bridge_openings) | show_bridge_openings %ni% c("yes", "no", "fake"))
+    stop("'show_bridge_openings' must be 'yes', 'no' or 'fake'")
+}
