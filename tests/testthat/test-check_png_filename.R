df1249bcbd960185be6e935a2ba7a77dd489f386
alexinedewit <a.s.de.wit@rug.nl>
2020-12-01 18:10:47 +0100

    Fix #99

---
+++ tests/testthat/test-check_png_filename.R
@@ -0,0 +1,34 @@
+
+
+test_that("check png_filename argument", {
+  expect_error(
+    create_map(
+      png_filename = 42
+    ),
+    "'png_filename' must be one word"
+  )
+  expect_error(
+    create_map(
+      png_filename = c("one filename", "too many")
+    ),
+    "'png_filename' must be one word"
+  )
+  expect_error(
+    create_map(
+      png_filename = NULL
+    ),
+    "'png_filename' must be one word"
+  )
+  expect_error(
+    create_map(
+      png_filename = c()
+    ),
+    "'png_filename' must be one word"
+  )
+  expect_error(
+    create_map(
+      png_filename = NA
+    ),
+    "'png_filename' must be one word"
+  )
+})
