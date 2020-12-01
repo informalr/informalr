3054a3dcacba2e4f780aa275a720704ad11351bc
Willy Tadema <friesewoudloper@gmail.com>
2020-12-01 19:19:19 +0100

    Hopefully a more elegent way to fix #79 using tidy evaluation idiom

--- R/create_map.R
+++ R/create_map.R
@@ -37,11 +37,9 @@ create_map <- function(
                  data$lat <= bbox["y", "max"] &
                  data$lon >= bbox["x", "min"] &
                  data$lon <= bbox["x", "max"], ]
-    lon <- NULL; rm(lon) # nolint, fixes warning: no visible binding for global variable
-    lat <- NULL; rm(lat) # nolint, fixes warning: no visible binding for global variable
     p <- p  +
       ggplot2::geom_point(data = data,
-                          ggplot2::aes(x = lon, y = lat),
+                          ggplot2::aes(x = .data[["lon"]], y = .data[["lat"]]),
                           colour = I("red"), size = I(3))
     }
   suppressMessages(ggplot2::ggsave(filename = png_filename, plot = p))
