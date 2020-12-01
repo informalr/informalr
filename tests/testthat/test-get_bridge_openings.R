4b077bdc65e800dd2989a791d6cbb012db284d0d
janclod <ctiecher@gmail.com>
2020-12-01 19:42:55 +0100

    Closes #96

--- tests/testthat/test-get_bridge_openings.R
+++ tests/testthat/test-get_bridge_openings.R
@@ -15,7 +15,6 @@ if (nrow(bridges) > 0) {
   })
 
   test_that("latitude and longitude are numerical", {
-    skip("Issue 96. Issue #96")
     expect_true(all(is.numeric(bridges$lat)))
     expect_true(all(is.numeric(bridges$lon)))
   })
@@ -23,7 +22,6 @@ if (nrow(bridges) > 0) {
 }
 
 test_that("no duplicate data", {
-  skip("Issue #83")
   bridges <- get_bridge_openings(fake_data = FALSE)
 
   # The first row should differ from the second ...
