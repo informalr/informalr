5813ec1a87ec9333772d422a25087b9db9ae253f
BaraW <barawaclawikova@gmail.com>
2020-12-01 18:46:48 +0100

    issue 31

--- tests/testthat/test-add_one.R
+++ tests/testthat/test-add_one.R
@@ -1,8 +1,6 @@
-add_one <- 42+1
-
 test_that("add_one", {
-  skip("Issue 42")
-  expect_equal(42 + 1, 43)
+  skip("Issue 31")
+  expect_equal(add_one(42, 1), 43)
 })
 
 
--- tests/testthat/test-get_baras_github_username.R
+++ tests/testthat/test-get_baras_github_username.R
@@ -3,9 +3,5 @@ test_that("use", {
   expect_equal("barawaclawik", get_baras_github_username())
 })
 
-add_one(42)
 
-add_one <- 42+1
-add_one
 
-usethis::use_test("add_one")
