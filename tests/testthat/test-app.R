938745bb3ef638803fcedc5469b90350e5ad2f0f
janclod <ctiecher@gmail.com>
2020-12-04 23:01:09 +0100

    Adding suffix to testApp()

--- tests/testthat/test-app.R
+++ tests/testthat/test-app.R
@@ -8,5 +8,6 @@ test_that("app() works", {
   # Use compareImages=FALSE because the expected image screenshots were created
   # on a Mac, and they will differ from screenshots taken on the CI platform,
   # which runs on Linux.
-  expect_pass(testApp("apps/app/", compareImages = grepl("^Ubuntu", utils::osVersion)))
+  app <- "apps/app/"
+  expect_pass(testApp(app, suffix = strsplit(utils::osVersion, " ")[[1]][1]))
 })
