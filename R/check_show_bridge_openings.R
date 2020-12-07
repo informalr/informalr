34d7749db8b925163f407c9307f91be3c5d01718
Willy Tadema <friesewoudloper@gmail.com>
2020-12-07 15:21:52 +0100

    Fix warning

--- R/check_show_bridge_openings.R
+++ R/check_show_bridge_openings.R
@@ -2,7 +2,9 @@
 #' @param show_bridge_openings character string indicating whether or not
 #' bridge openings are plotted on top of the basemap.
 #' @export
-check_show_bridge_openings <- function(a) {
-  if (!is.logical(a) || length(a) != 1 || is.na(a))
+check_show_bridge_openings <- function(show_bridge_openings) {
+  if (!is.logical(show_bridge_openings) ||
+      length(show_bridge_openings) != 1 ||
+      is.na(show_bridge_openings))
     stop("'show_bridge_openings' must be TRUE or FALSE")
 }
