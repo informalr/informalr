98d40e2275e989222cb0ce605d535f23f2c5035a
bogeert <bogeert@gmail.com>
2020-12-08 18:09:39 +0100

    #112b

--- R/create_map_measurements.R
+++ R/create_map_measurements.R
@@ -10,17 +10,18 @@ create_map_with_car_traffic_densities <- function(png_filename,
   bbox <- osmdata::getbb("Groningen", featuretype = "state")
   groningen <- ggmap::get_map(bbox, maptype = "toner_stamen")
   p <- ggmap::ggmap(groningen)
-  if(show_measurements) {
+  if (show_measurements) {
     data <- NULL; rm(data) # nolint, fixes warning: no visible  binding
     data <- informalr::get_car_densities()
     data$lat <- as.numeric(data$lat)
     data$lon <- as.numeric(data$lon)
     data$car_densities <- as.numeric(data$car_densities)
-    data$car_densities <- data$car_densities/30
+    data$car_densities <- data$car_densities / 30
     p <- p  +
       ggplot2::geom_point(data = data,
                           ggplot2::aes(x = data$lon, y = data$lat),
-                          colour = I("red"), size = data$car_densities)}
+                          colour = I("red"), size = data$car_densities)
+                          }
   #colour = I("red"), size = I(3), label = data$value)}
   ggplot2::ggsave(filename = png_filename, plot = p)
 }
