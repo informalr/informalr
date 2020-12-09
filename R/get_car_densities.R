9a925e53eeefe0c64ad3ff0b94f2b4b0c73b9167
Willy Tadema <friesewoudloper@gmail.com>
2020-12-09 14:29:31 +0100

    Fix merge errors

--- R/get_car_densities.R
+++ R/get_car_densities.R
@@ -1,59 +1,61 @@
 #' Get current measurements of traffic
 #'
+#' @return
 #' @export
+#'
+#' @examples
+#' \dontrun{
+#' get_car_densities()
+#' }
 get_car_densities <- function() {
-  #' download the positions of the measurement loops from NDW site
+  # Download the positions of the measurement loops from NDW site
   url <- "http://opendata.ndw.nu/measurement_current.xml.gz"
   destdir <- tempdir()
   if (!file.exists(destdir)) dir.create(destdir, recursive = TRUE)
   destfile <- file.path(destdir, "measurement_current.xml.gz")
-  #' unzip the file
   utils::download.file(url, destfile)
   tempfile <- tempfile()
   R.utils::gunzip(destfile, tempfile)
   doc <- xml2::read_xml(tempfile)
   file.remove(tempfile)
-  #find parent nodes in the XML with all the measurement points
+  # Find parent nodes in the XML with all the measurement points
   parents <- xml2::xml_find_all(doc, ".//d1:measurementSiteRecord")
-  #Get object values, longitudes and site names
-  #'value_query <-".//d1:measurementSiteName/d1:values/d1:value"
+  # Get object values, longitudes and site names
   lon_q <- ".//d1:measurementSiteLocation//d1:locationForDisplay/d1:longitude"
   lat_q <- ".//d1:measurementSiteLocation//d1:locationForDisplay/d1:latitude"
-  #'value<-xml2::xml_find_first(parents, value_query) %>% xml2::xml_text()
-  site_names_1 <- xml2::xml_find_all(doc,
-                                   ".//d1:measurementSiteRecord") %>%
-                                   xml2::xml_attr("id")
+  site_names_1 <- xml2::xml_attr(
+    xml2::xml_find_all(doc, ".//d1:measurementSiteRecord")
+  , "id")
   lon <- xml2::xml_text(xml2::xml_find_first(parents, lon_q))
   lat <- xml2::xml_text(xml2::xml_find_first(parents, lat_q))
-  #' now download the second file with all the traffic densities
+  # Now download the second file with all the traffic densities
   url <- "http://opendata.ndw.nu/trafficspeed.xml.gz"
   destdir <- tempdir()
   if (!file.exists(destdir)) dir.create(destdir, recursive = TRUE)
   destfile <- file.path(destdir, "trafficspeed.xml.gz")
   utils::download.file(url, destfile)
   tempfile <- tempfile()
-  #' unzip this file as well
+  # Unzip this file as well
   R.utils::gunzip(destfile, tempfile)
   doc <- xml2::read_xml(tempfile)
   file.remove(tempfile)
-  #'now extract the location names and the traffic densities
-  #' the following part is still incorrect,
-  #' because it finds one more location than density
-  #' and because it only finds the density on one position (rijbaan, weghelft)
-  site_names_2 <- xml2::xml_find_all(
-    doc,
-    ".//d1:siteMeasurements/d1:measurementSiteReference"
-  ) %>%
-    xml2::xml_attr("id")
+  # Now extract the location names and the traffic densities
+  # The following part is still incorrect,
+  # because it finds one more location than density
+  # and because it only finds the density on one position (rijbaan, weghelft)
+  site_names_2 <- xml2::xml_attr(
+    xml2::xml_find_all(doc, ".//d1:siteMeasurements/d1:measurementSiteReference")
+  , "id")
   dens_q <- paste0(".//d1:siteMeasurements/d1:measuredValue[1]/",
                    "d1:measuredValue/d1:basicData/d1:vehicleFlow/",
                    "d1:vehicleFlowRate")
-  density <- xml2::xml_find_all(doc, dens_q) %>%
-    xml2::xml_text()
-  #' this part is terribly slow
-  #' if the whole thing is done (with for(i in site_names_1)),
-  #' that is why I currently only check the first 1000
-  #'
+  density <- xml2::xml_text(
+    xml2::xml_find_all(doc, dens_q)
+  )
+
+  # This part is terribly slow
+  # if the whole thing is done (with for(i in site_names_1)),
+  # that is why I currently only check the first 1000
   for (i in 1:1000) {
     for (j in 1:1000) {
       if (isTRUE(identical(site_names_2[j], site_names_1[i]))) {
@@ -65,6 +67,6 @@ get_car_densities <- function() {
     }
   }
   car_densities <- site_names_1
-  #return the daya frame
+  # Return the dataframe
   data.frame(car_densities = car_densities, lon = lon, lat = lat)
 }
