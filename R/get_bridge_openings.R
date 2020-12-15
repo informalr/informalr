2f567ebe0e6b39df2e020221c75ebe07de900b51
Willy Tadema <friesewoudloper@gmail.com>
2020-12-15 19:27:49 +0100

    Fix error "could not find function %>%"

--- R/get_bridge_openings.R
+++ R/get_bridge_openings.R
@@ -2,7 +2,7 @@
 #'
 #' @param fake_data Indicates whether to return test data.
 #' (GPS location of the Martini tower.)
-#' @importFrom(magrittr,"%>%")
+#' @importFrom magrittr "%>%"
 #' @export
 #'
 #' @examples
